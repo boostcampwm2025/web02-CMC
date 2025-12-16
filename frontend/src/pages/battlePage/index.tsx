00768ffb12ddeb331aa730cbd756e9da30130562
JuJangGwon <tlsakch510@naver.com>
2025-12-16 18:09:00 +0900

    feat : 배틀 페이지 생성 및 기본 레이아웃 설계 - pages/battlePage/index.tsx로 기본 배틀 페이지 생성 - 헤더바 / 코드 블럭 / 채팅 / 이의제의 input / 투표 / 이의제기 & 반박 타임라인 컴포넌트 각각 어디에 배치할건지 임시 레이아웃 구성

---
+++ frontend/src/pages/battlePage/index.tsx
@@ -0,0 +1,19 @@
+export default function BattlePage() {
+  return (
+    <>
+      <header>
+        <h1>Battle Page</h1>
+        <div>타이머 / 상태</div>
+      </header>
+      <main className="flex">
+        <section>code</section>
+        <aside>
+          <section>chatting</section>
+          <section>이의제의 input</section>
+          <section>투표</section>
+        </aside>
+      </main>
+      <section>이의제기 & 반박 타임라인</section>
+    </>
+  );
+}
