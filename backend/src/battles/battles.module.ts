f9d7e39f67c07c30c109782c3fba5ab776421859
HoonDongKang <d159123@naver.com>
2025-12-16 17:04:14 +0900

    feat: Battle Module 추가

--- backend/src/battles/battles.module.ts
+++ backend/src/battles/battles.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { BattlesController } from './controller/battles.controller'
+import { BattlesGateway } from './battles.gateway'
 import { BattlesService } from './service/battles.service'
 
 @Module({
   controllers: [BattlesController],
-  providers: [BattlesService],
+  providers: [BattlesGateway, BattlesService],
 })
 export class BattlesModule {}
