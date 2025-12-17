db14f6d2ffe32a01eccce24e6eb0b6933529b67d
JuJangGwon <tlsakch510@naver.com>
2025-12-17 12:01:41 +0900

    feat: 메인 페이지, 팀 선 택 페이지, 배틀 생성 페이지 라우트 구성 - Mainpage (/) - 진영 선택 페이지 (/team-select) - 배틀 생성 페이지 (/battle/create)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/mainPage';
 import TeamSelectPage from './pages/teamSelectPage';
+import BattleCreatePage from './pages/battleCreatePage';
 import './App.css';
 
 function App() {
-  return <TeamSelectPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/team-select" element={<TeamSelectPage />} />
+        <Route path="/battle/create" element={<BattleCreatePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
