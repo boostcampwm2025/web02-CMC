944f63bf4579524583d359440b6a8b6065d8c6b6
HoonDongKang <d159123@naver.com>
2025-12-18 12:59:08 +0900

    feat: Battle Timer 추가 및 Phase 계산 로직 구현

--- backend/src/battles/service/battles.service.ts
+++ backend/src/battles/service/battles.service.ts
@@ -1,20 +1,23 @@
 import { v7 as uuidv7 } from 'uuid'
+import { EventEmitter } from 'node:events'
 import { Injectable, NotFoundException, BadRequestException, UnauthorizedException } from '@nestjs/common'
 
 import { mockBattleResults } from '../mock/battleResults.mock'
 import { TimelineItem, Mvp } from '../types/battleResult.types'
-import { ActiveBattleState, Battle, BattleTeam } from '../types/battles.types'
+import { ActiveBattleState, Battle, BattlePhase, BattleTeam } from '../types/battles.types'
 import { BattleResponseDto } from '../dto/battleResponse.dto'
 import { BattleResultResponseDto } from '../dto/battleResult.dto'
 import { BattleJoinRequestDto } from '../dto/battleJoinRequest.dto'
 import type { BattleCreateQueryDto } from '../dto/battleCreateQuery.dto'
 import { BattleJoinInfoResponseDto } from '../dto/battleJoinResponse.dto'
-import { BATTLE_PHASE, BATTLE_STATUS, BATTLE_TEAM, BATTLE_TYPE } from '../const/battles.const'
+import { BattlePhaseResponseDto, BattleRoundResponseDto, BattleTurnResponseDto } from '../dto/battleTurnResponse.dto'
+import { BATTLE_PHASE, BATTLE_PLAYTIME, BATTLE_STATUS, BATTLE_TEAM, BATTLE_TURN, BATTLE_TYPE } from '../const/battles.const'
 
 @Injectable()
-export class BattlesService {
+export class BattlesService extends EventEmitter {
   private battles: Battle[] = []
   private activeBattles: Map<string, ActiveBattleState> = new Map()
+  private battleTimers: Map<string, NodeJS.Timeout> = new Map()
 
   private generateId(): string {
     return uuidv7()
@@ -23,6 +26,7 @@ export class BattlesService {
   create(payload: BattleCreateQueryDto): Battle {
     const now = new Date()
     const battleId = this.generateId()
+    const playTime = BATTLE_PLAYTIME[payload.playTime]
 
     const battle: Battle = {
       id: battleId,
@@ -34,7 +38,7 @@ export class BattlesService {
       language: payload.language,
       type: payload.type,
       category: payload.category,
-      playTime: payload.playTime,
+      playTime,
       password: payload.type === BATTLE_TYPE.PRIVATE ? undefined : payload.password?.trim(),
       status: BATTLE_STATUS.OPEN,
       createdAt: now,
@@ -42,8 +46,8 @@ export class BattlesService {
       participantCount: 1,
       initialState: {
         round: 1,
-        phase: BATTLE_PHASE.WAITING_FOR_START,
-        timeRemainingSeconds: payload.playTime * 60,
+        phase: BATTLE_PHASE.OPINION_SHARE.name,
+        timeRemainingSeconds: playTime.time * 60,
       },
     }
 
@@ -164,6 +168,9 @@ export class BattlesService {
     if (!battleId) throw new BadRequestException('잘못된 요청입니다.')
     if (this.activeBattles.has(battleId)) return
 
+    const startedAt = Date.now()
+    const expiredAt = startedAt + BATTLE_PHASE.OPINION_SHARE.time
+
     const activeBattleState: ActiveBattleState = {
       battleId,
       all: {
@@ -186,13 +193,34 @@ export class BattlesService {
         attacks: [],
         defenses: [],
       },
+
+      phase: BATTLE_PHASE.OPINION_SHARE.name,
+      round: 1,
+      turn: null,
+
+      startedAt,
+      expiredAt,
     }
 
     this.activeBattles.set(battleId, activeBattleState)
+
+    const res = BattlePhaseResponseDto.of({
+      battleId,
+      phase: activeBattleState.phase,
+      startedAt: activeBattleState.startedAt,
+      expiredAt: activeBattleState.expiredAt,
+    })
+
+    this.emit('battle:phase:update', res)
+    this.scheduleNextTick(battleId)
   }
 
-  private addParticipant(battleId: string, clinetId: string, team: string): void {
-    if (!battleId || !clinetId || !team) throw new BadRequestException('잘못된 요청입니다.')
+  getBattleRoomId(battleId: string, team?: BattleTeam): string {
+    return team ? `battle:${battleId}:${team}` : `battle:${battleId}`
+  }
+
+  private addParticipant(battleId: string, clientId: string, team: string): void {
+    if (!battleId || !clientId || !team) throw new BadRequestException('잘못된 요청입니다.')
     const battleState = this.activeBattles.get(battleId)
 
     if (!battleState) throw new NotFoundException('해당 배틀은 현재 진행 중이지 않습니다.')
@@ -200,11 +228,163 @@ export class BattlesService {
     const { teamA, teamB } = battleState
 
     const myTeam = team === BATTLE_TEAM.A ? teamA : teamB
-    myTeam.users.push(clinetId)
+    myTeam.users.push(clientId)
   }
 
-  getBattleRoomId(battleId: string, team?: BattleTeam): string {
-    return team ? `battle:${battleId}:${team}` : `battle:${battleId}`
+  private updatePhase(battleId: string): void {
+    const state = this.getBattleState(battleId)
+    const battle = this.battles.find(battle => battle.id === battleId)
+    if (battle?.status === BATTLE_STATUS.CLOSED) return
+
+    const prevPhase = state.phase
+    const prevTurn = state.turn?.status ?? null
+    const prevRound = state.round
+
+    const now = Date.now()
+
+    const nextPhase = this.getNextPhase(state)
+    if (!nextPhase) return
+
+    state.phase = nextPhase.name
+    state.startedAt = now
+
+    if (!state.turn && nextPhase.time) {
+      state.expiredAt = now + nextPhase.time
+    }
+
+    if (prevPhase !== state.phase) {
+      const res = BattlePhaseResponseDto.of({
+        battleId,
+        phase: state.phase,
+        startedAt: state.startedAt,
+        expiredAt: state.expiredAt,
+      })
+      this.emit('battle:phase:update', res)
+    }
+
+    if (
+      state.turn?.status &&
+      !(prevPhase === BATTLE_PHASE.OPINION_SHARE.name && nextPhase === BATTLE_PHASE.TEAM_A_ATTACK) &&
+      prevTurn !== state.turn.status
+    ) {
+      const res = BattleTurnResponseDto.of({
+        battleId,
+        turn: state.turn,
+        startedAt: state.startedAt,
+        expiredAt: state.expiredAt,
+      })
+
+      this.emit('battle:turn:update', res)
+    }
+
+    if (prevRound !== state.round) {
+      const res = BattleRoundResponseDto.of({
+        battleId,
+        round: state.round,
+      })
+
+      this.emit('battle:round:update', res)
+    }
+
+    this.scheduleNextTick(battleId)
+  }
+
+  private getNextPhase(state: ActiveBattleState): BattlePhase | null {
+    switch (state.phase) {
+      case BATTLE_PHASE.OPINION_SHARE.name:
+        state.turn = {
+          status: BATTLE_TURN.A_ATTACK.name,
+          count: 1,
+        }
+        state.expiredAt = Date.now() + BATTLE_TURN.A_ATTACK.time
+        return BATTLE_PHASE.TEAM_A_ATTACK
+
+      case BATTLE_PHASE.TEAM_A_ATTACK.name:
+      case BATTLE_PHASE.TEAM_B_ATTACK.name:
+        return this.updateTurn(state)
+
+      case BATTLE_PHASE.TEAM_SWITCH.name: {
+        const isNextRound = this.updateRound(state)
+        return isNextRound ? BATTLE_PHASE.OPINION_SHARE : null
+      }
+
+      default:
+        return null
+    }
+  }
+
+  private updateTurn(state: ActiveBattleState): BattlePhase {
+    const now = Date.now()
+
+    if (!state.turn) {
+      state.turn = {
+        status: BATTLE_TURN.A_ATTACK.name,
+        count: 1,
+      }
+      state.expiredAt = now + BATTLE_TURN.A_ATTACK.time
+      return BATTLE_PHASE.TEAM_A_ATTACK
+    }
+
+    switch (state.turn.status) {
+      case BATTLE_TURN.A_ATTACK.name:
+        state.turn.status = BATTLE_TURN.B_DEFENSE.name
+        state.expiredAt = now + BATTLE_TURN.B_DEFENSE.time
+        return BATTLE_PHASE.TEAM_A_ATTACK
+
+      case BATTLE_TURN.B_DEFENSE.name:
+        if (state.turn.count < 2) {
+          state.turn.status = BATTLE_TURN.A_ATTACK.name
+          state.turn.count += 1
+          state.expiredAt = now + BATTLE_TURN.A_ATTACK.time
+          return BATTLE_PHASE.TEAM_A_ATTACK
+        } else {
+          state.turn.status = BATTLE_TURN.B_ATTACK.name
+          state.turn.count = 1
+          state.expiredAt = now + BATTLE_TURN.B_ATTACK.time
+          return BATTLE_PHASE.TEAM_B_ATTACK
+        }
+
+      case BATTLE_TURN.B_ATTACK.name:
+        state.turn.status = BATTLE_TURN.A_DEFENSE.name
+        state.expiredAt = now + BATTLE_TURN.A_DEFENSE.time
+        return BATTLE_PHASE.TEAM_B_ATTACK
+
+      case BATTLE_TURN.A_DEFENSE.name:
+        if (state.turn.count < 2) {
+          state.turn.status = BATTLE_TURN.B_ATTACK.name
+          state.turn.count += 1
+          state.expiredAt = now + BATTLE_TURN.B_ATTACK.time
+          return BATTLE_PHASE.TEAM_B_ATTACK
+        } else {
+          state.turn = null
+          return BATTLE_PHASE.TEAM_SWITCH
+        }
+    }
+  }
+
+  private updateRound(state: ActiveBattleState): boolean {
+    const battle = this.battles.find(battle => battle.id === state.battleId)
+    if (!battle) throw new NotFoundException('해당 배틀이 존재하지 않습니다.')
+
+    const nextRound = state.round + 1
+    const maxRounds = battle.playTime.rounds
+
+    if (maxRounds < nextRound) {
+      this.finishBattle(battle)
+      return false
+    }
+
+    state.round = nextRound
+    return true
+  }
+
+  private finishBattle(battle: Battle) {
+    //TODO 배틀 종료 처리
+    const battleTimer = this.battleTimers.get(battle.id)
+    clearTimeout(battleTimer)
+    this.battleTimers.delete(battle.id)
+    battle.status = BATTLE_STATUS.CLOSED
+    this.activeBattles.delete(battle.id)
   }
 
   private isPublicAndOpen(battle: Battle): boolean {
@@ -216,7 +396,7 @@ export class BattlesService {
   }
 
   private getExpiredTime(battle: Battle): Date {
-    return new Date(battle.createdAt.getTime() + battle.playTime * 60 * 1000)
+    return new Date(battle.createdAt.getTime() + battle.playTime.time * 60 * 1000)
   }
 
   private getBattleState(battleId: string) {
@@ -224,5 +404,23 @@ export class BattlesService {
     if (!battleState) throw new NotFoundException('해당 배틀은 현재 진행 중이지 않습니다.')
 
     return battleState
+  }
+
+  private scheduleNextTick(battleId: string) {
+    const battle = this.battles.find(battle => battle.id === battleId)
+    if (battle?.status === BATTLE_STATUS.CLOSED) return
+
+    const state = this.getBattleState(battleId)
+
+    const prevTimer = this.battleTimers.get(battleId)
+    if (prevTimer) clearTimeout(prevTimer)
+
+    const remaining = Math.max(state.expiredAt - Date.now(), 0)
+
+    const battleTimer = setTimeout(() => {
+      this.updatePhase(battleId)
+    }, remaining)
+
+    this.battleTimers.set(battleId, battleTimer)
   }
 }
