282a1bf05e0ee5c5f6766e1ba960a9f633849c4b
JangGwon <40955023+JuJangGwon@users.noreply.github.com>
2025-12-18 16:37:09 +0700

    [Feature] 배틀 채팅 UI 구현  (#47)
    * feat: 채팅 메시지 전송 기능 구현
    
    - ChatInput에 입력 상태 관리 및 전송 핸들러 추가 (소켓 통신은 미구현)
    - enter로도 채팅 이벤트 발생시킬 수 있도록 구현
    - ChatSection에 낙관적 업데이트 로직 구현
    
    * feat: 채팅 영역 스크롤 기능 구현 및 새 채팅 생길 시 자동 스크롤 기능 구현
    
    - 새 메시지 추가 시 채팅 스크롤이 자동으로 맨 아래로 이동
    - overflow-y-auto 속성 추가하여 스크롤바 활성화
    
    * style : 라운지 채팅 스크롤 디자인 변경
    - 기본 디자인에서 css 속성을 추가하여 디자인을 변경했습니다
    
    * style: 라운지 채팅 컴포넌트에 라운지 선택탭 UI 구현
    
    - 팀 라운지/전체 라운지 전환 탭 추가
    
    * feat: 라운지 채팅 탭별 메시지 관리 및 팀 뱃지 기능 추가
    
    - 팀/전체 라운지 메시지 상태 분리 및 독립 관리
    - 전체 라운지에서 팀 뱃지 표시 (A팀/B팀/중립)
    - useMemo를 이용하여 currentMessages 최적화 로직 적용
    
    * feat: 이의제기 및 반박 메시지 타입 추가 및 ObjectionMessage 컴포넌트 구현
    
    - ObjectionMessage 컴포넌트 신규 생성 (이의제기/반박 전용 UI)
    - ChatSection에서 메시지 타입별 조건부 렌더링 적용
    - 이의제기(빨간색), 반박(파란색) 구분 및 아이콘 표시
    
    * feat: 중립 사용자의 경우 전체 라운지만 렌더링 적용하는 로직 적용
    
    - 중립 사용자의 경우 전체 라운지만 표시 및 접근 가능
    - ChatTabs에서 중립 팀일 때 탭 전환 없이 전체 라운지 UI만 렌더링 하도록 로직 적용
    
    * fix : 병합하면서 통일 되지 않는 팀 정보 타입  수정
    - selectedTeam : A | B | none -> A | B| NONE 타입으로 수정

--- frontend/src/pages/battlePage/index.tsx
+++ frontend/src/pages/battlePage/index.tsx
@@ -49,7 +49,7 @@ export default function BattlePage() {
             codeB={battleInfo.bCode}
           />
           <aside className="flex flex-col gap-4">
-            <ChatSection aTeamMemebers={102} />
+            <ChatSection aTeamMemebers={102} team={selectedTeam} />
             <section>이의제의 input</section>
             <section>투표</section>
           </aside>
