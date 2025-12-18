88781ad9a6b253cb409efaeddc36824359e78544
USJUN <u.s.junn@gmail.com>
2025-12-18 09:38:10 +0900

    feature(be): Chat dto 추가

--- backend/src/battles/dto/battleJoinResponse.dto.ts
+++ backend/src/battles/dto/battleJoinResponse.dto.ts
@@ -19,8 +19,8 @@ export class BattleJoinResponseDto {
 
   static fromEntity(payload: ActiveBattleState, team: string): BattleJoinResponseDto {
     const res = new BattleJoinResponseDto()
-    const { all, teamA, teamB } = payload
-    const myTeam = team === BATTLE_TEAM.A ? teamA : teamB
+    const { all, teamA, teamB, teamNone } = payload
+    const myTeam = team === BATTLE_TEAM.A ? teamA : team === BATTLE_TEAM.B ? teamB : teamNone
 
     res.battleId = payload.battleId
     res.counts = {
