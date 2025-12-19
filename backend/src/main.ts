e16f28cb35bdc2416bbaf22be524322087cc385c
USJUN <u.s.junn@gmail.com>
2025-12-19 11:33:59 +0900

    devops: docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL
+      ? [process.env.FRONTEND_URL]
+      : ['http://localhost:5173', 'http://localhost', 'http://127.0.0.1:5173', 'http://127.0.0.1'],
     credentials: true,
   })
 
