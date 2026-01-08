bd6370a14f8d9f7a68df696e3f8e6928c8c0301f
wlstj <22020306jinseoyeon@gmail.com>
2026-01-08 10:39:51 +0900

    resolve conflict

--- frontend/src/pages/battlePage/index.tsx
+++ frontend/src/pages/battlePage/index.tsx
@@ -57,34 +57,32 @@ export default function BattlePage() {
         </div>
         <main className={`transition-all duration-300 ${isSidebarOpen ? 'main-width-open' : 'main-width-closed'}`}>
           <div className="flex gap-2 py-4">
-            <div className="flex gap-2 py-4">
-              <div className="flex-1">
-                <CodeSection
-                  onViewChange={setViewMode}
-                  currentView={viewMode}
-                  language="javascript"
-                  codeA={battleInfo.aCode}
-                  codeB={battleInfo.bCode}
-                />
-                <TimelineSection />
-              </div>
-              <aside className="flex flex-col gap-4 w-[590px]">
-                <DiscussionVote onVote={handleVote} />
-                <ChatSection />
-              </aside>
+            <div className="flex-1">
+              <CodeSection
+                onViewChange={setViewMode}
+                currentView={viewMode}
+                language={battleInfo.language}
+                codeA={battleInfo.aCode}
+                codeB={battleInfo.bCode}
+              />
+            </div>
+            <aside className="flex flex-col gap-4 w-[590px]">
+              <DiscussionVote onVote={handleVote} />
+              <ChatSection />
+            </aside>
           </div>
         </main>
 
-      {/* DiscussionInput 항상 중앙 하단에 고정 */}
-      <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[1800px] px-4 pb-4 z-50">
-        <div className="max-w-[590px] mx-auto">
-          <DiscussionInput onSubmit={handleDiscussionSubmit} />
+        {/* DiscussionInput 항상 중앙 하단에 고정 */}
+        <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[1800px] px-4 pb-4 z-50">
+          <div className="max-w-[590px] mx-auto">
+            <DiscussionInput onSubmit={handleDiscussionSubmit} />
+          </div>
         </div>
-      </div>
 
-      {isTeamChangeModalOpen && (
-        <TeamChangeModal handleTeamChange={handleTeamChange} onClose={handleCloseTeamChangeModal} />
-      )}
+        {isTeamChangeModalOpen && (
+          <TeamChangeModal handleTeamChange={handleTeamChange} onClose={handleCloseTeamChangeModal} />
+        )}
 
         {effectModal.isOpen && effectModal.team !== 'NONE' && (
           <DiscussionModal
