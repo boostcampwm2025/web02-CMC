deaee739498b99349fdbc7916257aa7ab4dc0235
JuJangGwon <tlsakch510@naver.com>
2026-01-09 12:01:02 +0900

    fix : 불필요한 파일 제거

--- frontend/src/pages/battlePage/components/tutorial/TutorialTeamChangeExample.tsx
+++
@@ -1,5 +0,0 @@
-import TeamChangeModalContent from '../modals/TeamChangeModalContent';
-
-export default function TutorialTeamChangeExample() {
-  return <TeamChangeModalContent currentTeam="A" teamACount={15} teamBCount={12} noneCount={3} remainingTime="02:30" />;
-}
