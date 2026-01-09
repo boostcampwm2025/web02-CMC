9d1ec0a42e2b1e182fb941a50aa64a5746aff949
JuJangGwon <tlsakch510@naver.com>
2026-01-09 12:02:36 +0900

    fix : 게임 페이즈 진행 시간 수정 (테스트용에서 게임 용으로 원복)

--- backend/src/battles/const/battles.const.ts
+++ backend/src/battles/const/battles.const.ts
@@ -5,15 +5,15 @@ export const BATTLE_PHASE = {
   },
   OPINION_SHARE: {
     name: 'OPINION_SHARE',
-    time: 1 * 1000, // 1분
+    time: 60 * 1000, // 1분
   },
   ATTACK: {
     name: 'ATTACK',
-    time: 8 * 1000, // 1분
+    time: 60 * 1000, // 1분
   },
   DEFENSE: {
     name: 'DEFENSE',
-    time: 8 * 1000, // 1분
+    time: 60 * 1000, // 1분
   },
   TEAM_SWITCH: {
     name: 'TEAM_SWITCH',
