e8d56f1f09484c7500431ca018b9e1ae9335611e
wlstj <22020306jinseoyeon@gmail.com>
2026-01-19 22:34:36 +0900

    chore: 환경변수 config 모듈 설치 #191

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { BattlesModule } from './battles/battles.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [BattlesModule],
+  imports: [BattlesModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
