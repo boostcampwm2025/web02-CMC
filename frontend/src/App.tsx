40c6c10cfc940de5b356dcb88398a8615209f43b
JuJangGwon <tlsakch510@naver.com>
2026-01-22 17:34:01 +0900

    feat : 에러 페이지 추가 및 라우트 errorElement 적용
    - 에러 페이지 구현
    - React Router ErrorElement 적용
    - 404/500 에러 코드별 메시지 분기처리 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,31 +11,41 @@ import NicknamePage from './pages/nicknamePage';
 import OAuthCallbackPage from './pages/OAuthCallbackPage';
 import { useAuthStore } from './commons/stores/authStore';
 import { ToastContainer } from './commons/components/toast/ToastContainer';
+import ErrorPage from './pages/errorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <MainPage />,
+    errorElement: <ErrorPage />,
     loader: async () => {
       // OAuth 로그인 후 리다이렉트 시 인증 상태 확인
       return await useAuthStore.getState().getOAuthUser();
     }
   },
   {
+    path: '/error',
+    element: <ErrorPage />
+  },
+  {
     path: '/auth/callback',
-    element: <OAuthCallbackPage />
+    element: <OAuthCallbackPage />,
+    errorElement: <ErrorPage />
   },
   {
     path: '/nickname',
-    element: <NicknamePage />
+    element: <NicknamePage />,
+    errorElement: <ErrorPage />
   },
   {
     path: '/login',
-    element: <LoginPage />
+    element: <LoginPage />,
+    errorElement: <ErrorPage />
   },
   {
     path: '/battle/:id',
     element: <BattlePage />,
+    errorElement: <ErrorPage />,
     loader: async ({ params }) => {
       const data = await fetchBattleInfo(params.id!);
       if (!data) throw new Response('Battle not found', { status: 404 });
@@ -45,6 +55,7 @@ const router = createBrowserRouter([
   {
     path: '/battle/:id/team-select/',
     element: <TeamSelectPage />,
+    errorElement: <ErrorPage />,
     loader: async ({ params }) => {
       const data = await fetchBattleInfo(params.id!);
       if (!data) throw new Response('Battle not found', { status: 404 });
@@ -53,11 +64,13 @@ const router = createBrowserRouter([
   },
   {
     path: '/battle/create',
-    element: <BattleCreatePage />
+    element: <BattleCreatePage />,
+    errorElement: <ErrorPage />
   },
   {
     path: '/battle/:id/result',
-    element: <BattleResultPage />
+    element: <BattleResultPage />,
+    errorElement: <ErrorPage />
   }
 ]);
 
