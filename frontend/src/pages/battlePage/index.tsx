a62f5bc32c98ff87ed6fa45adaf5e68b4ba919f6
JangGwon <40955023+JuJangGwon@users.noreply.github.com>
2026-01-23 09:15:21 +0700

    [HotFix] : SoundManager에 누락된 사운드 Preload 하는 로직 복구 (#238)
    fix : SoundManager에 사운드 Preload 하는 로직 누락 된 부분 복구

--- frontend/src/pages/battlePage/index.tsx
+++ frontend/src/pages/battlePage/index.tsx
@@ -59,6 +59,12 @@ export default function BattlePage() {
   // 사운드 초기화
   useEffect(() => {
     soundManager.preload('timerWarning', '/sounds/timerSound.wav');
+    soundManager.preload('notificationPing', '/sounds/notificationPing.mp3');
+    soundManager.preload('swoosh', '/sounds/swoosh.mp3');
+    soundManager.preload('swordSlash', '/sounds/swordSlash.mp3');
+    soundManager.preload('fanfare', '/sounds/fanfare.mp3');
+    soundManager.preload('click', '/sounds/click.mp3');
+    soundManager.preload('click2', '/sounds/click2.mp3');
   }, []);
 
   useEffect(() => {
