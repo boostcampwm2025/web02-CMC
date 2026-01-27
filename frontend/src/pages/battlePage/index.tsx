f343f2a5d1fca387d3fefe806fc46a84122ae37f
HoonDongKang <d159123@naver.com>
2026-01-27 15:55:21 +0900

    feat: 스킵 이벤트가 발생하면 스킵 모달 UI가 발생한다.

--- frontend/src/pages/battlePage/index.tsx
+++ frontend/src/pages/battlePage/index.tsx
@@ -23,6 +23,7 @@ import DiscussionModal from './components/effects/DiscussionModal';
 import BattleProgressBoard from './components/progressBoard/ProgressBoard';
 import TeamVoteResultModal from './components/effects/TeamVoteResultModal';
 import RoundUpdateModal from './components/effects/RoundUpdateModal';
+import SkipModal from './components/effects/SkipModal';
 import { selectUser, useAuthStore } from '@/commons/stores/authStore';
 
 export default function BattlePage() {
@@ -196,7 +197,6 @@ export default function BattlePage() {
             </aside>
           </div>
         </main>
-
         {/* DiscussionInput - 화면 중앙 하단에 fixed */}
         <div
           className={`fixed bottom-0 left-1/2 transform -translate-x-1/2 z-[5] px-4 pb-4 transition-all duration-500 ease-out ${
@@ -207,7 +207,6 @@ export default function BattlePage() {
             {shouldShowInput && <DiscussionInput key={phase} onSubmit={handleDiscussionSubmit} />}
           </div>
         </div>
-
         {isTeamChangeModalOpen && (
           <TeamChangeModal
             topics={battleInfo.topics}
@@ -215,7 +214,6 @@ export default function BattlePage() {
             onClose={handleCloseTeamChangeModal}
           />
         )}
-
         {effectModal.isOpen && effectModal.team !== 'NONE' && (
           <DiscussionModal
             isOpen={effectModal.isOpen}
@@ -225,7 +223,6 @@ export default function BattlePage() {
             onClose={hideEffect}
           />
         )}
-
         {isVoteResultModalOpen && voteResult && (
           <TeamVoteResultModal
             isOpen={isVoteResultModalOpen}
@@ -240,10 +237,11 @@ export default function BattlePage() {
             onClose={closeVoteResultModal}
           />
         )}
-
         {roundModal.isPending && !isVoteResultModalOpen && (
           <RoundUpdateModal isOpen={true} round={roundModal.round} topic={roundModal.topic} onClose={hideRoundEffect} />
         )}
+
+        <SkipModal isOpen={true} onClose={hideRoundEffect} />
 
         <TutorialModal
           isOpen={isTutorialOpen && currentStep === 'welcome'}
@@ -252,7 +250,6 @@ export default function BattlePage() {
           dontShowAgain={dontShowAgain}
           onDontShowAgainChange={setDontShowAgain}
         />
-
         <TutorialStepModal
           isOpen={isTutorialOpen && currentStep !== 'welcome' && currentStep !== 'completed'}
           currentStep={currentStep}
