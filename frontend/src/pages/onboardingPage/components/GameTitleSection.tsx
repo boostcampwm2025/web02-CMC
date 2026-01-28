3e93a806ee76fa363006d3c13138ba0946b600b7
JuJangGwon <tlsakch510@naver.com>
2026-01-29 02:56:58 +0900

    style : 온보딩 페이지 최상단 타이틀 섹션에 캐릭터 이미지 추가
    - 1.5초에 한번씩 흔들 흔들 흔들 거리는 애니메이션 추가

--- frontend/src/pages/onboardingPage/components/GameTitleSection.tsx
+++ frontend/src/pages/onboardingPage/components/GameTitleSection.tsx
@@ -1,6 +1,8 @@
 import { Play, ChevronDown } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import onBoardingBackgroundImage from '/public/images/gameLogo.png';
+import onBoardingPeoplesImage1 from '/public/images/onBoarding/onBoardingPeople1.png';
+import onBoardingPeoplesImage2 from '/public/images/onBoarding/onBoardingPeople2.png';
 
 export default function GameTitleSection() {
   const navigate = useNavigate();
@@ -18,16 +20,29 @@ export default function GameTitleSection() {
         <img
           src={onBoardingBackgroundImage}
           alt="CMC Background"
-          className="max-w-8xl w-full h-auto object-contain opacity-60"
+          className="absolute max-w-8xl w-full h-auto object-contain z-0"
         />
-        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-[#0a0b14]/30 to-[#0a0b14]" />
+        <div className="absolute bottom-[25%] min-[1600px]:bottom-[28%] min-[1920px]:bottom-[25%] left-1/2 -translate-x-1/2 flex gap-8 z-10">
+          <img
+            src={onBoardingPeoplesImage1}
+            alt="People Left"
+            className="w-[11rem] h-auto object-contain animate-character-wiggle"
+          />
+          <img
+            src={onBoardingPeoplesImage2}
+            alt="People Right"
+            className="w-[11rem] h-auto object-contain animate-character-wiggle"
+            style={{ animationDelay: '0.5s' }}
+          />
+        </div>
+        <div className="absolute bottom-0 left-0 right-0 h-[30%] bg-gradient-to-b from-transparent to-[#0a0b14] z-20" />
       </div>
 
-      <div className="relative z-10">
-        <div className="flex gap-8 items-center justify-center">
+      <div className="relative z-30">
+        <div className="flex gap-12 items-center justify-center">
           <button
             onClick={() => navigate('/')}
-            className="flex items-center gap-2 px-8 py-4 bg-gradient-to-r from-orange-500 to-red-600 rounded-xl cursor-pointer"
+            className="flex items-center justify-center gap-2 text-xl px-8 py-4 bg-gradient-to-r from-orange-500 to-red-600 border border-orange-300 rounded-xl cursor-pointer shadow-[0_10px_40px_rgba(251,146,60,0.6)]"
           >
             <Play fill="currentColor" size={20} />
             <span>입장하기</span>
@@ -35,7 +50,7 @@ export default function GameTitleSection() {
 
           <button
             onClick={handleScrollDown}
-            className="flex items-center gap-2 px-8 py-4 bg-[#1a1b26] rounded-xl cursor-pointer"
+            className="flex items-center justify-center text-xl gap-2 px-8 py-4 bg-[#1a1b26] border border-gray-600 rounded-xl cursor-pointer shadow-[0_10px_40px_rgba(0,0,0,0.7)]"
           >
             <span>더 알아보기</span>
             <ChevronDown size={20} />
