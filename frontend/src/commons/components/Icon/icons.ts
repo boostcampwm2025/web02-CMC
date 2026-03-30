6f887fe6f58e063c6f58f29a4c60204e35684689
JuJangGwon <tlsakch510@naver.com>
2026-03-30 21:21:09 +0900

    feat : 공용 Icon 컴포넌트 구현
    - svg/png 호환 가능하도록 구현
    - name prop으로 icon을 결정할 수 있도록 구현
    - svg icon들을 lazy loading 해도 트리셰이킹이 안되기에 정적으로 임포트 하는 방식으로 구현

---
+++ frontend/src/commons/components/Icon/icons.ts
@@ -0,0 +1,90 @@
+import BattleIcon from '@/assets/icon/battle.svg?react';
+import CheckIcon from '@/assets/icon/check.svg?react';
+import ClockIcon from '@/assets/icon/clock.svg?react';
+import CloseIcon from '@/assets/icon/close.svg?react';
+import CopyIcon from '@/assets/icon/copy.svg?react';
+import CrownIcon from '@/assets/icon/crown.svg?react';
+import DevIcon from '@/assets/icon/dev.svg?react';
+import DownArrowIcon from '@/assets/icon/downArrow.svg?react';
+import ExclamationIcon from '@/assets/icon/exclamation.svg?react';
+import EyeIcon from '@/assets/icon/eye.svg?react';
+import GithubIcon from '@/assets/icon/github.svg?react';
+import KakaoIcon from '@/assets/icon/kakao.svg?react';
+import LikeIcon from '@/assets/icon/like.svg?react';
+import MessageIcon from '@/assets/icon/message.svg?react';
+import PauseIcon from '@/assets/icon/pause.svg?react';
+import PeopleIcon from '@/assets/icon/people.svg?react';
+import PeoplesIcon from '@/assets/icon/peoples.svg?react';
+import PlayIcon from '@/assets/icon/play.svg?react';
+import PlusIcon from '@/assets/icon/plus.svg?react';
+import QuestionIcon from '@/assets/icon/question.svg?react';
+import ScaleIcon from '@/assets/icon/scale.svg?react';
+import SendIcon from '@/assets/icon/send.svg?react';
+import ShieldIcon from '@/assets/icon/shield.svg?react';
+import SkipIcon from '@/assets/icon/skip.svg?react';
+import SoundIcon from '@/assets/icon/sound.svg?react';
+import SwitchIcon from '@/assets/icon/switch.svg?react';
+import TimelineIcon from '@/assets/icon/timeline.svg?react';
+import TimerIcon from '@/assets/icon/timer.svg?react';
+import TrophyIcon from '@/assets/icon/trophy.svg?react';
+import UserIcon from '@/assets/icon/user.svg?react';
+import VoteIcon from '@/assets/icon/vote.svg?react';
+import WorldIcon from '@/assets/icon/world.svg?react';
+
+import BronzePng from '@/assets/icon/bronze.png';
+import SilverPng from '@/assets/icon/silver.png';
+import GoldPng from '@/assets/icon/gold.png';
+import DiamondPng from '@/assets/icon/diamond.png';
+import MasterPng from '@/assets/icon/master.png';
+import GrandmasterPng from '@/assets/icon/grandmaster.png';
+
+import type { FC, SVGProps } from 'react';
+
+export type SvgIconComponent = FC<SVGProps<SVGSVGElement>>;
+
+export const SVG_ICONS = {
+  battle: BattleIcon,
+  check: CheckIcon,
+  clock: ClockIcon,
+  close: CloseIcon,
+  copy: CopyIcon,
+  crown: CrownIcon,
+  dev: DevIcon,
+  downArrow: DownArrowIcon,
+  exclamation: ExclamationIcon,
+  eye: EyeIcon,
+  github: GithubIcon,
+  kakao: KakaoIcon,
+  like: LikeIcon,
+  message: MessageIcon,
+  pause: PauseIcon,
+  people: PeopleIcon,
+  peoples: PeoplesIcon,
+  play: PlayIcon,
+  plus: PlusIcon,
+  question: QuestionIcon,
+  scale: ScaleIcon,
+  send: SendIcon,
+  shield: ShieldIcon,
+  skip: SkipIcon,
+  sound: SoundIcon,
+  switch: SwitchIcon,
+  timeline: TimelineIcon,
+  timer: TimerIcon,
+  trophy: TrophyIcon,
+  user: UserIcon,
+  vote: VoteIcon,
+  world: WorldIcon
+} as const satisfies Record<string, SvgIconComponent>;
+
+export const TIER_ICONS = {
+  bronze: BronzePng,
+  silver: SilverPng,
+  gold: GoldPng,
+  diamond: DiamondPng,
+  master: MasterPng,
+  grandmaster: GrandmasterPng
+} as const satisfies Record<string, string>;
+
+export type IconName = keyof typeof SVG_ICONS;
+export type TierName = keyof typeof TIER_ICONS;
